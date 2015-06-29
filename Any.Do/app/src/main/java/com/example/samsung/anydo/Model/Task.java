1eee62a440e905f383d26a303e547b7767d3e959
xidianfushuai <1048367400@qq.com>
2015-06-29 16:57:56 +0800

    add models

--- Any.Do/app/src/main/java/com/example/samsung/anydo/Model/Task.java
+++ Any.Do/app/src/main/java/com/example/samsung/anydo/Model/Task.java
@@ -1,7 +1,29 @@
 package com.example.samsung.anydo.Model;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by samsung on 29/06/2015.
  */
 public class Task {
+    private String name;
+    private Date time;
+    private List<String> list
+    public Task(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String str){
+        this.name=name;
+    }
+    public Date getTime(){
+        return time;
+    }
+    public void setTime(Date time){
+        this.time=time;
+    }
+
 }
